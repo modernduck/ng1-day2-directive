cf94578c46ffd3a71a9b063aadf22c01857bc853
Sompop Kulapalanont <sompopkulapalanont@Sompops-MacBook-Pro.local>
2017-05-24 10:47:07 +0700

    directive

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
   angular.module('myApp', [])
             .controller("MainController", function($scope){
              
-                $scope.customer1 = {
-                    name : "Sompop",
-                    address : "Bangkok far far away"
-                }
+                $scope.customers = [{
+                        name : "Sompop",
+                        lastname:"Kulapalanont",
+                        picture:"http://placehold.it/100x100",
+                        address : "Bangkok far far away"
+                    },
+                    {
+                        name : "Doodle",
+                        lastname:"Kulapalanont",
+                        picture:"http://placehold.it/130x130",
+                        address : "Doobe doo be doo",
+                        isPrivate:true
+                    },
+                    {
+                        name : "Doom DOom",
+                        lastname:"Kulapalanont",
+                        picture:"http://placehold.it/200x200",
+                        address : "Dooom di doom di doom"
+                    }
+                ]
 
-                $scope.customer2 = {
-                    name : "Doodle",
-                    address : "Doobe doo be doo",
-                    isPrivate:true
-                }
+                
 
-                $scope.customer3 = {
-                    name : "Doom DOom",
-                    address : "Dooom di doom di doom"
-                }
-
+                
             } )
             .directive('myCustomer', function(){
                 //return directive object
@@ -29,5 +37,18 @@
                     template:"<div>Name {{customerInfo.name}}</div><div ng-hide='customerInfo.isPrivate'> address {{customerInfo.address}}</div>"
 
                 }
+
+            })
+            .directive('customerList', function(){
+                return {
+                    scope:{
+                        customerName:'=name',
+                        customerPicture:'=picture',
+                        customerLastname:'=lastname',
+                        customerInfo:'=info',
+                    },
+                    template:'<div class="row"><div class="col-md-2"><img src="{{customerPicture}}"></div><div class="col-md-10"><h3>{{customerName}} {{customerLastname}}</h3><p>{{customerInfo}}</p></div></div>'
+                }
+
 
             })
